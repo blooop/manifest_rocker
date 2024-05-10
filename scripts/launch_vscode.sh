785f6fcf7c6269cb41b725a0bd5aed30ed64d073
Austin Gregg-Smith <blooop@gmail.com>
2024-05-10 12:25:18 +0100

    update launch vscode to use branch name

--- scripts/launch_vscode.sh
+++ scripts/launch_vscode.sh
@@ -11,7 +11,7 @@ cd "$SCRIPT_DIR"/..
 
 git submodule update --init --recursive
 
-CONTAINER_NAME=${PWD##*/}  
+CONTAINER_NAME="$(basename "$(pwd)")-$(git rev-parse --abbrev-ref HEAD)"
 
 echo "stopping existing container" "$CONTAINER_NAME" 
 docker stop "$CONTAINER_NAME" || true 
@@ -19,9 +19,6 @@ docker stop "$CONTAINER_NAME" || true
 CONTAINER_HEX=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
 
 rocker --nvidia --x11 --user --pull --git --image-name "$CONTAINER_NAME" --name "$CONTAINER_NAME" --volume "${PWD}":/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" --deps-dependencies ubuntu:22.04 
-
-# docker pull ghcr.io/red5d/docker-autocompose:latest
-# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose $CONTAINER_NAME > .devcontainer/docker-compose.yaml
 
 #this follows the same convention as if it were opened by a vscode devcontainer
 code --folder-uri vscode-remote://attached-container+"$CONTAINER_HEX"/workspaces/"${CONTAINER_NAME}"
