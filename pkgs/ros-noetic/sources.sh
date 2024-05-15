2c0da5ac6aa4210bbc1b843a5bce30d9d9a8dcea
Austin Gregg-Smith <blooop@gmail.com>
2024-05-15 17:04:50 +0100

    [add] basic ros noetic

---
+++ pkgs/ros-noetic/sources.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Add PPA or apt sources, or source code
+echo 'Etc/UTC' > /etc/timezone && \
+    ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime && \
+    apt-get update && \
+    apt-get install -q -y --no-install-recommends tzdata && \
+    rm -rf /var/lib/apt/lists/*
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+echo "deb http://packages.ros.org/ros/ubuntu focal main" > /etc/apt/sources.list.d/ros1-latest.list
