35d789a4eee8bac89807b287da10957686fa5e16
Austin Gregg-Smith <blooop@gmail.com>
2024-05-17 15:58:12 +0100

    update urdf-viz

--- pkgs/urdf-viz/install_rustup.sh
+++ pkgs/urdf-viz/install_rustup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+. "$HOME/.cargo/env"
--- pkgs/urdf-viz/install_urdf_viz.sh
+++ pkgs/urdf-viz/install_urdf_viz.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+. "$HOME/.cargo/env"
+
+
 cargo install urdf-viz --features assimp
