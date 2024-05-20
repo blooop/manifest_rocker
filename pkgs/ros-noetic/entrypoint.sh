1553f9d5574cd206478291619c2ea0f2bcfb1f33
Austin Gregg-Smith <blooop@gmail.com>
2024-05-20 16:23:40 +0100

    update ros noetic

---
+++ pkgs/ros-noetic/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+echo -e "source /opt/ros/noetic/setup.bash" >> $HOME/.bashrc
--- scripts/launch_vscode.sh
+++ scripts/launch_vscode.sh
@@ -18,7 +18,7 @@ docker stop "$CONTAINER_NAME" || true
 
 CONTAINER_HEX=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
 
-rocker --nvidia --x11 --user --pull --git --image-name "$CONTAINER_NAME" --name "$CONTAINER_NAME" --volume "${PWD}":/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" --deps-dependencies ubuntu:focal
+rocker --nvidia --x11 --user --pull --git --image-name "$CONTAINER_NAME" --name "$CONTAINER_NAME" --volume "${PWD}":/workspaces/"${CONTAINER_NAME}":Z --deps --oyr-run-arg " --detach" ubuntu:focal
 
 #this follows the same convention as if it were opened by a vscode devcontainer
-code --folder-uri vscode-remote://attached-container+"$CONTAINER_HEX"/workspaces/"${CONTAINER_NAME}"
+code --folder-uri vscode-remote://attached-container+"$CONTAINER_HEX"/workspaces/"${CONTAINER_NAME}"    
