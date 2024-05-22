8a6f2b575d542398255e81af5895fe7dc4c2c023
Austin Gregg-Smith <blooop@gmail.com>
2024-05-22 19:39:35 +0100

    add example

---
+++ pkgs/build123d/example.py
@@ -0,0 +1,11 @@
+from build123d import *
+from ocp_vscode import show
+
+import build123d as bd
+
+with bd.BuildPart() as bp:
+    bd.Box(3, 3, 3)
+    with bd.BuildSketch(*bp.faces()):
+        bd.Rectangle(1, 2, rotation=45)
+    bd.extrude(amount=0.1)
+show(bp)
